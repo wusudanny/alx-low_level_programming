f775ae626cee5fccb38e621fc84e58d2157d8a89
Wusu Daniel <wusudanny@gmail.com>
2022-10-17 12:55:22 -0700

    advanced

---
+++ 6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	return (0);
+}
